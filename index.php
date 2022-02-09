43832f647bc0610b128f1d9be30d4a25357cbedc
Erigzon <bislimierigzon10@gmail.com>
2022-02-09 21:06:06 +0100

    index

---
+++ index.php
@@ -0,0 +1,30 @@
+
+<?php require 'header.php' ?>
+
+<nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+	<button class="navbar-toggler" type="button" data-toggle="collapse" 
+	data-tagert="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" 
+	aria-expanded="false" aria-label="Toggle navigation">
+		<span class="navbar-toggler-icon"></span>
+	</button>
+
+	<div class="collapse navbar-collapse" id="navbarsExampleDefault">
+		<ul class="navbar-nav">
+			<li class="nav-item active">
+				<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="login.php">Login</a>
+			</li>
+		</ul>
+	</div>
+</nav>
+
+<main role="main" class="container">
+	
+
+</main>
+
+
+<?php require 'footer.php' ?>
+
