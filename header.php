a8fc1a6c88660c8ecac7faf54511549a0030517b
ArgjendEjupi1 <ae52893@ubt-uni.net>
2022-02-09 21:30:25 +0100

    header

---
+++ header.php
@@ -0,0 +1,15 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Home</title>
+
+	<!-- Bootstrap code CSS -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<!-- Custom CSS -->
+	<link rel="stylesheet" href="css/main.css">
+
+
+</head>
+<body class="text-center">
